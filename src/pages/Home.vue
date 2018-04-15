3327df2e71fcf7ec2275a1313a20e2e388a2d325
avivharuzi <avivharuzi@gmail.com>
2018-04-16 00:04:40 +0300

    Added basic files to project

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'home page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
