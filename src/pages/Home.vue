535aee3d2b7ab068c502cfdfea7a564db82ec667
avivharuzi <avivharuzi@gmail.com>
2018-04-23 17:17:18 +0300

    changed: files

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,2 @@
 <template>
-  <div>
-    <p>{{ header }}</p>
-  </div>
 </template>
-
-<script>
-export default {
-  name: 'Home',
-  data () {
-    return {
-      header: 'this is home page'
-    }
-  }
-}
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// Pages
 import Home from '@/pages/Home'
 
 Vue.use(Router)
 
+// Routes
 export default new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Home',
       component: Home
     }
   ]
