0adfc8c410c495872e995c01c5d8b4f49cd4ee20
avivharuzi <avivharuzi@gmail.com>
2018-05-11 13:14:03 +0300

    update filters

--- src/filters/capitalize.js
+++ src/filters/capitalize.js
@@ -1,8 +1,4 @@
 export function capitalize(value) {
-  if (value) {
-    value = value.toLowerCase()
-    return value.charAt(0).toUpperCase() + value.slice(1)
-  } else {
-    return value
-  }
+  value = value.toLowerCase()
+  return value.charAt(0).toUpperCase() + value.slice(1)
 }
--- src/filters/ucwords.js
+++ src/filters/ucwords.js
@@ -1,17 +1,13 @@
 export function ucwords(value) {
-  if (value) {
-    let newValue = ''
-    let words = value.split(' ')
-    let space = ' '
-    for (let i = 0; i < words.length; i++) {
-      if (i === words.length - 1) {
-        space = ''
-      }
-      words[i] = words[i].toLowerCase()
-      newValue += words[i].charAt(0).toUpperCase() + words[i].slice(1) + space
+  let newValue = ''
+  let words = value.split(' ')
+  let space = ' '
+  for (let i = 0; i < words.length; i++) {
+    if (i === words.length - 1) {
+      space = ''
     }
-    return newValue
-  } else {
-    return value
+    words[i] = words[i].toLowerCase()
+    newValue += words[i].charAt(0).toUpperCase() + words[i].slice(1) + space
   }
+  return newValue
 }
