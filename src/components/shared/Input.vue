590895d0f2af0202f8e36d86866e9ae02fb687a9
avivharuzi <avivharuzi@gmail.com>
2018-05-17 18:16:04 +0300

    added input component

---
+++ src/components/shared/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-group">
+    <label>{{ label }}</label>
+    <input :type="type" v-bind:value="value"
+    v-on:input="$emit('input', $event.target.value)"
+    class="form-control" :class="[size ? 'form-control-' + size : null, error ? 'is-invalid' : null]"
+    :placeholder="label">
+    <small v-if="message" class="form-text text-muted">{{ message }}</small>
+    <div v-if="error" class="invalid-feedback">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['label', 'type', 'value', 'size', 'message', 'error'],
+    data () {
+      return {
+        type: 'text'
+      }
+    }
+  }
+</script>
