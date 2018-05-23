26f4b1ebf0fd8bdf75841293af6c380e21510cc1
avivharuzi <avivharuzi@gmail.com>
2018-05-23 13:53:18 +0300

    more fixes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from '@/App'
 
-import store from '@/store'
 import router from '@/router'
+import store from '@/store'
 import filters from '@/filters'
 
 import '@/scss/main.scss'
